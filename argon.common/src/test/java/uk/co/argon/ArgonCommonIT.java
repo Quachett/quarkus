9e6de845450f2fd1c12652032bfdf5a8197d05c9
Quachett <qauchett@gmail.com>
2022-05-22 18:14:01 +0200

    ficed

--- argon.common/src/test/java/uk/co/argon/ArgonCommonIT.java
+++
@@ -1,9 +0,0 @@
-package uk.co.argon;
-
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-
-@QuarkusIntegrationTest
-public class ArgonCommonIT extends ArgonCommonTest {
-
-    // Execute the same tests but in native mode.
-}
