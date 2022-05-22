dc9dcc71a0ca80aa656f8605390a936800bf3d6e
Quachett <quachett@gmail.com>
2022-05-22 16:40:23 +0200

    Add files via upload

---
+++ argon.common/src/test/java/uk/co/argon/ArgonCommonIT.java
@@ -0,0 +1,9 @@
+package uk.co.argon;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class ArgonCommonIT extends ArgonCommonTest {
+
+    // Execute the same tests but in native mode.
+}
